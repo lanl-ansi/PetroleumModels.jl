15a2254d2097e9d19ed3ef8897855d1659d8d165
elennnaaa <45135698+elennnaaa@users.noreply.github.com>
2020-08-14 08:59:38 -0600

    Add files via upload

--- src/PetroleumModels.jl
+++ src/PetroleumModels.jl
@@ -1,45 +1,55 @@
-module PetroleumModels
 
-    import JSON
-    import JuMP
-    import InfrastructureModels
-#    using Compat
-    import Memento
+    using InfrastructureModels
+    const _IM = InfrastructureModels
 
+    import InfrastructureModels: ids, ref, var, con, sol, nw_ids, nws, optimize_model!, @im_fields, ismultinetwork
 
+    using JSON
+    using JuMP
+    using Memento
+    using Printf
+    using MathOptInterface
 
-    # Create our module level logger (this will get precompiled)
-    const LOGGER = Memento.getlogger(PetroleumModels)
+    using Dates
+    using Dierckx
 
-    # Register the module level logger at runtime so that folks can access the logger via `getlogger(PetroleumModels)`
-    # NOTE: If this line is not included then the precompiled `PetroleumModels.LOGGER` won't be registered at runtime.
-    __init__() = Memento.register(LOGGER)
-
-    "Suppresses information and warning messages output by PetroleumModels, for fine grained control use the Memento package"
-    function silence()
-    info(LOGGER, "Suppressing information and warning messages for the rest of this session.  Use the Memento package for more fine-grained control of logging.")
-        setlevel!(getlogger(InfrastructureModels), "error")
-        setlevel!(getlogger(PetroleumModels), "error")
-    end
-    import MathOptInterface
     const MOI = MathOptInterface
     const MOIU = MathOptInterface.Utilities
 
-    const _pm_global_keys = Set(["time_series_block", "per_unit", "nu", "base_h_loss", "rho", "gravitational_acceleration",
+    # Create our module level logger (this will get precompiled)
+    const _LOGGER = Memento.getlogger(@__MODULE__)
+
+    # Register the module level logger at runtime so that folks can access the logger via `getlogger(PetroleumModels)`
+    # NOTE: If this line is not included then the precompiled `PetroleumModels.LOGGER` won't be registered at runtime.
+    __init__() = Memento.register(_LOGGER)
+
+    "Suppresses information and warning messages output by PetroleumModels, for fine grained control use the Memento package"
+    function silence()
+        Memento.info(_LOGGER, "Suppressing information and warning messages for the rest of this session.  Use the Memento package for more fine-grained control of logging.")
+        Memento.setlevel!(Memento.getlogger(InfrastructureModels), "error")
+        Memento.setlevel!(Memento.getlogger(PetroleumModels), "error")
+    end
+
+    "alows the user to set the logging level without the need to add Memento"
+    function logger_config!(level)
+        Memento.config!(Memento.getlogger("PetroleumModels"), level)
+    end
+
+
+const _pm_global_keys = Set(["time_series_block", "per_unit", "nu", "base_h_loss", "rho", "gravitational_acceleration",
 "baseH", "baseQ", "base_a", "base_z", "base_b"])
+include("core/base.jl")
+include("core/data.jl")
+# include("core/types.jl")
+include("core/constraint.jl")
+include("core/constraint_template.jl")
+include("core/objective.jl")
+include("core/solution.jl")
+include("core/variable.jl")
+include("core/common.jl")
+include("io/grail.jl")
+include("core/misocp.jl")
 
-    include("core/data.jl")
-    include("core/base.jl")
-    include("core/constraint.jl")
-    include("core/constraint_template.jl")
-    include("core/objective.jl")
-    include("core/solution.jl")
-    include("core/variable.jl")
-    include("core/common.jl")
-# include("core/type.jl")
-    include("core/misocp.jl")
+include("io/matl.jl")
 
-    include("io/matl.jl")
-
-    include("prob/ls.jl")
-end
+include("prob/ls.jl")
